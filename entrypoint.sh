a7e0b7abf1c2d4c0c14c6716e2dea209c1fceeab
HoraceZhang <impressioncr@hotmail.com>
2025-01-24 10:43:28 +0800

    pool mainnet

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ ETH_ADRRDESS=${ETH_ADRRDESS:-"0x640713d8feb0c75e828c12d8682343cc8ea56666"}
 # 当前机器角色：主机（"master"）或从机（"worker"）
 WORK_NAME=${WORK_NAME:-"worker"}
 
-POOL=${POOL:"pool-core-testnet.inichain.com:32672"}
+POOL=${POOL:"pool-a.yatespool.com:31588"}
 
 ./iniminer --pool stratum+tcp://${ETH_ADRRDESS}.${WORK_NAME}@${POOL}
